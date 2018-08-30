728d69df6389dcdb6f118191e05e2e012e9040ae
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-30 10:49:29 +0300

    created page

---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='1df1134531e84a1211bb5dd6a03843c6'
+export SECRET_KEY='<Flask WTF Secret Key>'
+
+python3.6 manage.py server
---
+++ tests/test_review.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import Movie
+
+class ReviewTest(unittest.TestCase):
+  '''
+  Test class to test the behaviour of the Review class
+  '''
+
+  def setUp(self):
+    '''
+    Set up method that will run before every test
+    '''
+    self.new_review = Review()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_review,Review))
+
+if __name__ == '__main__':
+  unittest.main()
