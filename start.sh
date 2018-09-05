d350e0e7ef5e4e0dd62b220010828996740e357f
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-05 20:41:35 +0300

    built project

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='1df1134531e84a1211bb5dd6a03843c6'
-export SECRET_KEY='<Flask WTF Secret Key>'
+export SECRET_KEY='gL0711'
+export MAIL_USERNAME=giftlumumba2@gmail.com
+export MAIL_PASSWORD=gL0711419032
 
 python3.6 manage.py server
